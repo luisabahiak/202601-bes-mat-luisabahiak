368c686761b07385700cd75267c1eb1c6bdd9051
Luisa Bahia <luisabkat@gmail.com>
2026-04-01 01:44:44 -0300

    Criacao Controllers e Menu

--- src/main/java/br/com/ucsal/olimpiadas/repository/ProvaRepository.java
+++ src/main/java/br/com/ucsal/olimpiadas/repository/ProvaRepository.java
@@ -7,28 +7,6 @@ import br.com.ucsal.olimpiadas.Prova;
 
 public class ProvaRepository extends BaseRepository<Prova> {
 	
-	/*private final List<Prova> provas = new ArrayList<>();
-	private long proximaProvaId = 1;
-	
-	public Prova salvarProva(Prova p)
-	{
-		p.setId(proximaProvaId++);
-		provas.add(p);
-		return p;
-	}
-	
-	public List<Prova> listar() {
-		return provas;
-	}
-	
-	public Prova buscarPorId(long id) {
-		for(Prova p : provas) {
-			if(id == p.getId()) {
-				return p;
-			}
-		}
-		return null;		
-	}
-	*/
+
 	
 }
